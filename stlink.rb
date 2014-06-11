b349f307b63e809145058b9d1f9d734714f8d1e2
Lukas Schauer <lukas@schauer.so>
2014-06-11 20:03:00 +0200

    added version and url and homepage and stuff to stlink.rb

--- stlink.rb
+++ stlink.rb
@@ -1,8 +1,12 @@
 require "formula"
 
 class Stlink < Formula
-  homepage ""
+  homepage "https://github.com/texane/stlink"
   head "https://github.com/texane/stlink.git"
+
+  version "1.0.0"
+  url "https://github.com/texane/stlink/archive/1.0.0.tar.gz"
+  sha1 "d55bbdd8c4c907be15b28d089fddc86e7a167766"
 
   depends_on "automake"
   depends_on "autoconf"
